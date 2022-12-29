fa7f2e1a4c3ef1b43108c75160229f33703721e7
Oliver Epper <oliver.epper@gmail.com>
2022-12-29 11:36:12 +0100

    additional export for the original csv format

--- CMS50F_Cli/main.c
+++ CMS50F_Cli/main.c
@@ -41,6 +41,15 @@ static void print(FILE *stream, time_t *timestamp, spo2_t spo2, bpm_t bpm)
     fprintf(stream, "%s, spo: %d, bpm: %d\n", buffer, spo2, bpm);
 }
 
+static void print_orig_format(FILE *stream, time_t *timestamp, spo2_t spo2, bpm_t bpm)
+{
+    char date_buffer[16] = {0};
+    strftime(date_buffer, sizeof(date_buffer), "%Y-%m-%d", localtime(timestamp));
+    char time_buffer[16] = {0};
+    strftime(time_buffer, sizeof(time_buffer), "%H:%M:%S", localtime(timestamp));
+    fprintf(stream, "%s, %s, %d, %d\n", date_buffer, time_buffer, spo2, bpm);
+}
+
 static void print_to_stdout(time_t *timestamp, spo2_t spo2, bpm_t bpm, unsigned rest)
 {
     print(stdout, timestamp, spo2, bpm);
@@ -68,10 +77,33 @@ static void print_to_file(time_t *timestamp, spo2_t spo2, bpm_t bpm, unsigned re
     }
 }
 
-static void print_both(time_t *timestamp, spo2_t spo2, bpm_t bpm, unsigned rest)
+static void print_to_csv_file(time_t *timestamp, spo2_t spo2, bpm_t bpm, unsigned rest)
+{
+    static FILE *out = {0};
+    if (out == 0) {
+        char buffer[32] = {0};
+        char fullpath[PATH_MAX];
+        strftime(buffer, sizeof(buffer), "%Y%m%d%H%M%S.csv", localtime(timestamp));
+        if ((out = fopen(buffer, "w")) == NULL) {
+            LOG_ERROR("could not open outputfile: %s", buffer);
+        }
+        realpath(buffer, fullpath);
+        LOG_DEBUG("outputfile %s opened", fullpath);
+        fprintf(out, "DATE,TIME,SPO2,PULSE\n");
+
+    }
+    print_orig_format(out, timestamp, spo2, bpm);
+    if (rest == 0) {
+        if (fclose(out) == EOF) LOG_ERROR("could not close file: %s", strerror(errno));
+        else LOG_DEBUG("%s", "file closed");
+    }
+}
+
+static void print_all(time_t *timestamp, spo2_t spo2, bpm_t bpm, unsigned rest)
 {
     print_to_stdout(timestamp, spo2, bpm, rest);
     print_to_file(timestamp, spo2, bpm, rest);
+    print_to_csv_file(timestamp, spo2, bpm, rest);
 }
 
 void die(cms50f_device_t device, cms50f_status_t status) {
@@ -106,7 +138,7 @@ int main(void) {
     if (status != CMS50F_SUCCESS) die(device, status);
     else printf("Starttime: %s", asctime(localtime(&starttime)));
 
-    status = cms50f_storage_data(device, duration, starttime, &print_both);
+    status = cms50f_storage_data(device, duration, starttime, &print_all);
     if (status != CMS50F_SUCCESS) die(device, status);
 
     cms50f_device_destroy(&device);
